1a8747cd5ce89c7e7d3efefc3ad36065a1382cb3
anoushameh <42008584+anoushameh@users.noreply.github.com>
2018-08-17 10:18:16 -0400

    adding files for webpage

---
+++ stuffForBlog.js
@@ -0,0 +1,9 @@
+
+function buttonFunc() {
+  document.getElementById('contactInfo').innerHTML='click the links below & my social media/accounts are listed: instagram:__anousha__//listen to my playlists:spotify:anousha mehta';
+}
+
+  function festivalsFunc() {
+
+    document.getElementById('music').innerHTML='1. Coachella @ Coachella Valley, California <br> 2. Lollapalooza @ Grant Park, Chicago <br> 3. Bonnaroo @ Nashville, Tennessee <br> 4. Ultra Music Festival @ Miami, FL <br> 5. Electric Daisy Carnival @ Las Vegas';
+  }
